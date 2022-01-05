982704f05ff4a164d60823b5503677f8a43654f4
NanoSymmetry <nanosymmetry@gmail.com>
2022-01-05 09:05:05 -0500

    Made file selector work

--- download.sh
+++ download.sh
@@ -16,4 +16,4 @@ done
 yt-dlp --download-archive downloaded_videos.txt --no-warning --progress \
 -o "channel-dl/%(uploader)s [%(channel_id)s]/%(upload_date)s - %(title)s - (%(duration)ss) [%(id)s].%(ext)s" --merge-output mkv --check-formats \
 --write-info-json --write-description --write-thumbnail --add-metadata --all-subs \
---batch-file="$file"
+--batch-file=$(pwd)/channels/"$file"
