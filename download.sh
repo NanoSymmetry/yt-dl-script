07c3a20055b0b9e01153ea50fdb718606b8cace2
NanoSymmetry <nanosymmetry@gmail.com>
2022-01-05 09:22:59 -0500

    All now works

--- download.sh
+++ download.sh
@@ -8,7 +8,7 @@ availableFiles="${availableFiles##*/}"
 echo "Available files: (-1 to exit: 'all' for all)"
 select file in $availableFiles; do
     if [[ $REPLY == '-1' ]]; then exit; fi
-    if [[ $REPLY == 'all' ]]; then file='channels/*'; fi
+    if [[ $REPLY == 'all' ]]; then file='temp.txt'; cat channels/* >> channels/temp.txt; fi
     break
 done
 
@@ -17,3 +17,5 @@ yt-dlp --download-archive downloaded_videos.txt --no-warning --progress \
 -o "channel-dl/%(uploader)s [%(channel_id)s]/%(upload_date)s - %(title)s - (%(duration)ss) [%(id)s].%(ext)s" --merge-output mkv --check-formats \
 --write-info-json --write-description --write-thumbnail --add-metadata --all-subs \
 --batch-file=$(pwd)/channels/"$file"
+
+if [[ -e channels/temp.txt ]]; then rm channels/temp.txt; fi
