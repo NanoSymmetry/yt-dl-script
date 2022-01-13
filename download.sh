0e384b0e8f61f4fa8028598fcf4691fa21a85076
NanoSymmetry <nanosymmetry@gmail.com>
2022-01-13 11:38:04 -0500

    made selection more efficient

--- download.sh
+++ download.sh
@@ -1,10 +1,9 @@
 #!/bin/env bash
 
-#Get available files
+# Get available files
 availableFiles=$(ls channels/)
-availableFiles="${availableFiles##*/}"
 
-#Get user selection
+# Get user selection
 echo "Available files: (-1 to exit: 'all' for all)"
 select file in $availableFiles; do
     if [[ $REPLY == '-1' ]]; then exit; fi
@@ -12,10 +11,11 @@ select file in $availableFiles; do
     break
 done
 
-#Run yt-dlp to download
+# Run yt-dlp to download
 yt-dlp --download-archive downloaded_videos.txt --no-warning --progress \
 -o "channel-dl/%(uploader)s [%(channel_id)s]/%(upload_date)s - %(title)s - (%(duration)ss) [%(id)s].%(ext)s" --merge-output mkv --check-formats \
 --write-info-json --write-description --write-thumbnail --add-metadata --all-subs \
 --batch-file=$(pwd)/channels/"$file"
 
+# Remove temp batch file if it exists
 if [[ -e channels/temp.txt ]]; then rm channels/temp.txt; fi
